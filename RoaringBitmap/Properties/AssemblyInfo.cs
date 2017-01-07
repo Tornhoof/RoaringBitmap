05b5b420c8fce71d7aeff9592f832fe6d9772b0a
Tornhoof <tornhoof@gmail.com>
2017-01-07 22:38:39 +0100

    It's 2017 ;)

--- RoaringBitmap/Properties/AssemblyInfo.cs
+++ RoaringBitmap/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RoaringBitmap Contributors")]
 [assembly: AssemblyProduct("Roaring Bitmap")]
-[assembly: AssemblyCopyright("Copyright © Roaring Bitmap Contributors 2016")]
+[assembly: AssemblyCopyright("Copyright © Roaring Bitmap Contributors 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
